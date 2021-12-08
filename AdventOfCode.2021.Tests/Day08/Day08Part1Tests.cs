1044041455c9c8330cc20bb021bb7ecaabbfb489
Rico Brase <kontakt@rico-brase.de>
2021-12-08 11:14:22 +0100

    Day08: Added part 1.

---
+++ AdventOfCode.2021.Tests/Day08/Day08Part1Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode._2021.Day08;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode._2021.Tests.Day08
+{
+    public class Day08Part1Tests
+    {
+        private static readonly string[] SampleInput = {
+            "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+            "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+            "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+            "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+            "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+            "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+            "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+            "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+            "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+            "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+        };
+
+        [Fact]
+        public void ParseInput_ShouldReturnCorrectlyParsedData()
+        {
+            // arrange
+            var countOfEntries = SampleInput.Length;
+            const int countOfElementsInSignalPatterns = 10;
+            const int countOfElementsInOutputValue = 4;
+
+            // act
+            var parsedData = Day08Common.ParseInput(SampleInput);
+
+            // assert
+            parsedData.Should().HaveCount(countOfEntries); 
+            foreach (var (signalPatterns, outputValues) in parsedData)
+            {
+                signalPatterns.Should().HaveCount(countOfElementsInSignalPatterns);
+                outputValues.Should().HaveCount(countOfElementsInOutputValue);
+            }
+        }
+
+        [Fact]
+        public void CountSimpleDigits_Part1_ShouldReturnCorrectCount()
+        {
+            // arrange
+            const int expectedCount = 26;
+            var inputData = Day08Common.ParseInput(SampleInput);
+            
+            // act
+            var actualCount = Day08Common.CountSimpleDigits_Part1(inputData);
+            
+            // assert
+            actualCount.Should().Be(expectedCount);
+        }
+
+    }
+
+}
---
+++ AdventOfCode.2021/Day08/Day08Common.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2021.Day08
+{
+    public static class Day08Common
+    {
+
+        public static List<(List<string> signalPatterns, List<string> outputValues)> ParseInput(string[] input)
+        {
+            return input.Select(ParseLine).ToList();
+        }
+
+        private static (List<string> signalPatterns, List<string> outputValues) ParseLine(string input)
+        {
+            var lineData = input
+                .Split('|')
+                .Select(it => it
+                    .Trim()
+                    .Split(' ')
+                    .ToList()
+                )
+                .ToList();
+            
+            return (lineData.First(), lineData.Last());
+        }
+
+        public static int CountSimpleDigits_Part1(
+            List<(List<string> signalPatterns, List<string> outputValues)> listOfInputs)
+        {
+            return listOfInputs
+                .Select(it => it.outputValues)
+                .SelectMany(it => it)
+                .Select(it => it.Length)
+                .Count(it => it is 2 or 4 or 3 or 7);
+        }
+        
+    }
+}
